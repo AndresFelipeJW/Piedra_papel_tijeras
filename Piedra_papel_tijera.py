b53b2b213b46b104a2a764ede2575a76f3904350
sistemas <you@example.com>
2023-03-23 15:40:04 -0500

    Se agrego el diagrama de flujo, programa y el README

--- Piedra_papel_tijera.py
+++ Piedra_papel_tijera.py
@@ -1,41 +1,57 @@
-# Programa para simular el juego piedra papel o tijera
-print("--------------------------------------")
-print("-------PIEDRA PAEL O TIJERA-----------")
-print("--------------------------------------")
+# Piedra, papel o tijera
+
 import random
 
-# processing 
-maquina = random.randint(1,3)
+print("--------------------------------------------")
+print("-----------Piedra, papel o tijera-----------")
+print("--------------------------------------------")
 
-# maquina = random.randint(1,3)
+print("1 Corresponde a piedra")
+print("2 Corresponde a papel")
+print("3 Corresponde a tijera")
 
-jugador = int(input("DIGITE LA OPCION:"))
+# input
 
-if (maquina==1): # PIEDRA
-    if (jugador==1):
-        Rta = "Empate"
-    elif (jugador==2):
-        Rta = "Ganaste!"
-    else:
-        Rta = "PERDISTE"
-
-if (maquina==2): # PAPEL
-    if (jugador==2):
-        Rta = "Empate"
-    elif (jugador==3):
-        Rta = "Ganaste!"
-    else: 
-        Rta = "PERDISTE"
-
-if (maquina==3): # TIJERAS
-    if (jugador==3):
-        Rta = "Empate"
-    elif (jugador==1):
-        Rta = "Ganaste!"
-    else:
-        Rta = "PERDISTE"
-
+j = int(input(" TU ELECCIÓN : "))
     
+# processing
 
-# output
-print
+m = random.randint(1,3)
+
+if (j == 1):  #piedra
+    if (m == 1):
+        print("Empate")
+        print("Elegiste piedra y la máquina sacó piedra")
+    elif (m==2):
+        print("Lo siento, perdiste")
+        print("Elegiste piedra y la máquina saco papel")
+    else:
+        print("Felicidades, ganaste")
+        print("Elegiste piedra y la máquina sacó tijera")
+
+        
+if (j == 2): #papel
+    if (m == 1):
+        print("Felicidades, ganaste")
+        print("Elegiste papel y la máquina sacó piedra")
+    elif (m == 2):
+        print("Empate")
+        print("Elegiste papel y la máquina sacó papel")
+    else:
+       print ("Lo siento, perdiste")
+       print("Elegiste papel  y la máquina sacó tijera")
+
+
+if (j == 3): #tijera
+    if ( m == 1):
+        print("Lo siento, perdiste")
+        print("Elegiste tijera y la máquina sacó piedra")
+    elif ( m ==2):
+        print("Felicidades, ganaste")
+        print("Elegiste tijera y la máquina sacó papel")
+    else:
+        print("Empate")
+        print("Elegiste tijera y la máquina sacó tijera")
+
+if (j > 3):
+    print("Ingresaste una opcion inválida, revisa las opciones")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Piedra_papel_tijeras
 Programa para simular el juego piedra paple o tijera 
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
